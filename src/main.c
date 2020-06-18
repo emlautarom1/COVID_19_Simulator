d051daa29c7909562e4b6ace15c305368386645c
emlautarom1 <emlautarom1@gmail.com>
2020-06-18 16:14:28 -0300

    Added optional GUI flag

--- src/main.c
+++ src/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <time.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_timer.h>
 
-#define DEBUG true
+#define DEBUG false
 
 #define WIN_W 600
 #define WIN_H 600
@@ -19,13 +20,21 @@
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 3)
+    unsigned int MY_RANDOM_SEED =
+#if defined(DEBUG) && DEBUG
+        31415926;
+#else
+        (unsigned int)time(NULL);
+#endif
+
+    if (argc < 4)
     {
-        fprintf(stderr, "Usage: %s <rows> <cols>", argv[0]);
+        fprintf(stderr, "Usage: %s <rows> <cols> <t|f>", argv[0]);
         return -1;
     }
     int rows = atoi(argv[1]);
     int cols = atoi(argv[2]);
+    bool use_gui = (*argv[3]) == 't';
 
     if (rows < 2 || cols < 2)
     {
@@ -33,34 +42,39 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+    SDL_Window *window;
+    SDL_Renderer *rend;
+    if (use_gui)
     {
-        printf("Error initializing SDL: %s\n", SDL_GetError());
-        return -1;
-    }
-    SDL_Window *window = SDL_CreateWindow("COVID-19 Simulator",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          WIN_W, WIN_H, 0);
-    if (!window)
-    {
-        printf("Error creating main window: %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
+        DEBUG_PRINT("Using SDL2 as GUI\n");
+        if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+        {
+            printf("Error initializing SDL: %s\n", SDL_GetError());
+            return -1;
+        }
+        window = SDL_CreateWindow("COVID-19 Simulator",
+                                  SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED,
+                                  WIN_W, WIN_H, 0);
+        if (!window)
+        {
+            printf("Error creating main window: %s\n", SDL_GetError());
+            SDL_Quit();
+            return -1;
+        }
 
-    SDL_Renderer *rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-
-    if (!rend)
-    {
-        printf("Error creating renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
+        rend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (!rend)
+        {
+            printf("Error creating renderer: %s\n", SDL_GetError());
+            SDL_DestroyWindow(window);
+            SDL_Quit();
+            return -1;
+        }
     }
 
     // Init random number generation
-    srand(__rand_seed__);
+    srand(MY_RANDOM_SEED);
 
     Cell *matrix = malloc((size_t)(cols * rows) * sizeof(Cell));
     Cell *upd_matrix = malloc((size_t)(cols * rows) * sizeof(Cell));
@@ -71,47 +85,50 @@ int main(int argc, char const *argv[])
     Uint32 sim_speed = 10;
     for (int sim_t = 0; sim_t < SIM_LIMIT; sim_t++)
     {
-        // Handle events
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
+        if (use_gui)
         {
-            switch (event.type)
+            // Handle events
+            SDL_Event event;
+            while (SDL_PollEvent(&event))
             {
-            case SDL_QUIT:
-                sim_t = SIM_LIMIT;
-                break;
-            case SDL_KEYDOWN:
-                if (event.key.keysym.scancode == SDL_SCANCODE_RIGHTBRACKET)
-                    sim_speed = MIN(MAX_SPEED, sim_speed + 1);
-                if (event.key.keysym.scancode == SDL_SCANCODE_LEFTBRACKET)
-                    sim_speed = MAX(sim_speed - 1, 1);
-                if (event.key.keysym.scancode == SDL_SCANCODE_Q)
+                switch (event.type)
+                {
+                case SDL_QUIT:
                     sim_t = SIM_LIMIT;
-            default:
-                break;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.scancode == SDL_SCANCODE_RIGHTBRACKET)
+                        sim_speed = MIN(MAX_SPEED, sim_speed + 1);
+                    if (event.key.keysym.scancode == SDL_SCANCODE_LEFTBRACKET)
+                        sim_speed = MAX(sim_speed - 1, 1);
+                    if (event.key.keysym.scancode == SDL_SCANCODE_Q)
+                        sim_t = SIM_LIMIT;
+                default:
+                    break;
+                }
             }
-        }
 
-        // Rendering
-        SDL_Rect rect = {.w = CELL_SIZE, .h = CELL_SIZE};
-        SDL_RenderClear(rend);
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
+            // Rendering
+            SDL_Rect rect = {.w = CELL_SIZE, .h = CELL_SIZE};
+            SDL_RenderClear(rend);
+            for (int i = 0; i < rows; i++)
             {
-                CellStatus current_status = matrix[i * cols + j].status;
-                rect.x = j * CELL_SIZE;
-                rect.y = i * CELL_SIZE;
+                for (int j = 0; j < cols; j++)
+                {
+                    CellStatus current_status = matrix[i * cols + j].status;
+                    rect.x = j * CELL_SIZE;
+                    rect.y = i * CELL_SIZE;
 
-                SDL_SetRenderDrawColor(rend,
-                                       (current_status >> 16) & 0xFF,
-                                       (current_status >> 8) & 0xFF,
-                                       current_status & 0xFF,
-                                       255);
-                SDL_RenderFillRect(rend, &rect);
+                    SDL_SetRenderDrawColor(rend,
+                                           (current_status >> 16) & 0xFF,
+                                           (current_status >> 8) & 0xFF,
+                                           current_status & 0xFF,
+                                           255);
+                    SDL_RenderFillRect(rend, &rect);
+                }
             }
+            SDL_RenderPresent(rend);
         }
-        SDL_RenderPresent(rend);
 
         // Update
         memcpy(upd_matrix, matrix, (size_t)(cols * rows) * sizeof(Cell));
@@ -147,7 +164,8 @@ int main(int argc, char const *argv[])
         // Debugging
         DEBUG_PRINT("\n\tTime: %d\n\tSpeed: %d\n", sim_t, sim_speed);
 
-        SDL_Delay(1000 / sim_speed);
+        if (use_gui)
+            SDL_Delay(1000 / sim_speed);
     }
 
     DEBUG_PRINT("Simulation finished!\n");
@@ -155,9 +173,13 @@ int main(int argc, char const *argv[])
     // Cleanup
     free(matrix);
     free(upd_matrix);
-    SDL_DestroyRenderer(rend);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+
+    if (use_gui)
+    {
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
 
     return 0;
 }
